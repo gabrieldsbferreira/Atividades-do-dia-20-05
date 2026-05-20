9131beeade5835674f79c68833d03f4339b33d6e
gabrieldsbferreira <00001091993427sp@al.educacao.sp.gov.br>
2026-05-20 08:33:37 -0300

    Create Atividade2.sql

---
+++ Atividade2.sql
@@ -0,0 +1,5 @@
+A saída do exemplo tá errada. "Conta" não existe na entrada.
+
+Pela regra do enunciado, o resultado certo é: Vontade, Jane, Projeto de Lei e Zack.
+
+Query: `SELECT nome FROM Customer WHERE id_do_referente IS NULL OR id_do_referente != 2;`
